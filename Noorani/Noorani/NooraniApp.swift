f154f314d6d6cd237baf259aaff3c386c14ff7b5
amin <amin@safelemons.com>
2025-09-14 21:44:34 -0400

    initial project setup

---
+++ Noorani/Noorani/NooraniApp.swift
@@ -0,0 +1,17 @@
+//
+//  NooraniApp.swift
+//  Noorani
+//
+//  Created by Amin Pourgol on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct NooraniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Noorani/NooraniTests/NooraniTests.swift
@@ -0,0 +1,16 @@
+//
+//  NooraniTests.swift
+//  NooraniTests
+//
+//  Created by Amin Pourgol on 9/14/25.
+//
+
+import Testing
+
+struct NooraniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
